81a3a12e0a21dc3576e0c6c30e4616bbc7ca255f
Hamid Reza Pakpour <h.pakpour@DOTIN.IR>
2025-02-12 09:41:34 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_i_am_rich_sample
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_i_am_rich_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_i_am_rich_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
